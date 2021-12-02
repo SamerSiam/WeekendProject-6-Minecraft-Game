0a754385dce1e7ebb492bade104de2140e7578de
SamerSiam <ssiam05@gmail.com>
2021-12-03 01:17:40 +0200

    implementing one tool

--- minecraft.js
+++ minecraft.js
@@ -1,4 +1,35 @@
 const SKY_ELEMENT=0;
+const CLOUD_ELEMENT=1;
+const TREE_ELEMENT=4;
+const TRUNK_ELEMENT=5;
+const ROCK_ELEMENT=7;
+const GRASS_ELEMENT=8;
+const GROUND_ELEMENT=9;
+let minedTypes=[];
+let selectedTool={};
+
+/** define tool objects, each will have ID, name, elements */
+
+const tools=[
+    {
+        id:"1",
+        name:"axe",
+        elements:["tree", "trunk"]
+    },
+    {
+        id:"2",
+        name:"pickaxe",
+        elements:["rock"]
+    },
+
+    {
+        id:"3",
+        name:"shovel",
+        elements:["grass","ground"]
+    },
+
+
+];
 const initialMatrix = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -22,6 +53,8 @@ const initialMatrix = [
     [9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9],
 ];
 const game=document.querySelector('.game-matrix-grid')
+const toolPickaxe=document.querySelector('#pickaxe');
+console.log(toolPickaxe.getAttribute("data-id"));
 
 for (let row=0; row< initialMatrix.length; row++){
     for (let col=0; col< initialMatrix.length; col++)
@@ -38,6 +71,7 @@ for (let row=0; row< initialMatrix.length; row++){
             break;
             case 7: createElement('rock');
             break;
+
             case 6:
             case 4: 
             createElement('tree');
@@ -48,17 +82,37 @@ for (let row=0; row< initialMatrix.length; row++){
     }
 }
 function createElement(type){
-    let skyDiv=document.createElement('div');
-    skyDiv.classList.add(type);
-    skyDiv.addEventListener('click',gridClick);
-    game.appendChild(skyDiv);
-   
+    let blockDiv=document.createElement('div');
+    blockDiv.classList.add(type);
+    blockDiv.addEventListener('click',gridClick);
+    game.appendChild(blockDiv); 
 }
 
 function gridClick(e)
 {
-    e.target.setAttribute('class','sky' );
-    // e.target.classList.add('sky');
+    type='sky';
+    const className= e.target.getAttribute('class');
+    console.log(className);
+    if (minedTypes.includes(className)){
+        e.target.setAttribute('class','');
+        e.target.classList.add(type);
+    }
+    else{
+        console.log(selectedTool);
+        // selectedTool.classList.add('wrong-tool');
+    }
+   
 }
+
+
+toolPickaxe.addEventListener('click', ()=>{
+    toolPickaxe.classList.add('selected-tool');
+    const typeId=toolPickaxe.getAttribute("data-id");
+    selectedTool=tools.find(tool => tool.id===typeId );
+    minedTypes=selectedTool.elements;
+    // gridMine(minedTypes);
+    console.log(minedTypes);
+    console.log(selectedTool);
+});
 
 
